90c05c7a4bf4cbf4d1d957b3fa5bbf39febe5e07
Ariel Ayala <arielayalamail@gmail.com>
2020-01-19 17:29:26 -0300

    Agegado del about.html

--- index.py
+++ index.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    #return "Hola mundo !"
+    # return "Hola mundo !"
     return render_template('inicio.html')
+
 
 @app.route('/about')
 def about():
-    print ("ingreso en la ruta about")
-    return "About"
+    print("ingreso en la ruta about")
+    #return "About"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
